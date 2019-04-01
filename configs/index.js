1f0b2598e55910ad8aef3880cc0574ce2728f5f4
Aluno <Aluno@Digital.net>
2019-04-01 20:52:21 -0300

    Coments

--- configs/index.js
+++ configs/index.js
@@ -1,26 +1,46 @@
-var express = require('express');
+var express = require('express'); //Recupera o modulo Serial express
 
 var app = express();
-var server = app.listen(4000, () => { //Start the server, listening on port 4000.
-    console.log("Listening to requests on port 4000...");
+
+var path = require('path'); //Recupera o modulo path, que vem junto do express
+
+var server = app.listen(4000, () => { //Inicia o servidor na porta 4000
+    console.log("Recebendo solicitações na porta 4000...");
 })
 
-var io = require('socket.io')(server); //Bind socket.io to our express server.
+var io = require('socket.io')(server); //Recupera o modulo so socket.io e atrela o socket.io ao nosso servidor express.
+
 
 app.use(express.static('public')); //Send index.html page on GET /
 
-const SerialPort = require('serialport'); 
-const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort('COM7'); //Connect serial port to port COM3. Because my Arduino Board is connected on port COM3. See yours on Arduino IDE -> Tools -> Port
-const parser = port.pipe(new Readline({delimiter: '\n'})); //Read the line only when new line comes.
-parser.on('data', (temp) => { //Read data
-    console.log(temp);
-    var datahora = new Date();
-    var data = datahora.getDate()+"/"+(Number(datahora.getMonth())+1)+"/"+datahora.getFullYear();
-    var hora = (datahora.getHours())+":"+(datahora.getMinutes());
-    io.sockets.emit('temp', {date: data, time: hora, temp:temp}); //emit the datd i.e. {date, time, temp} to all the connected clients.
+
+
+app.get('/', (req, res) => { //Simplesmente devolve a index.html quando for digitado no navegador localhost:4000
+    res.sendFile('index.html', {
+        root: path.join(__dirname, '..')
+    })
+})
+
+const SerialPort = require('serialport'); //Recupera o modulo Serial Port
+
+const Readline = SerialPort.parsers.Readline; // Atribui o metodo readline do serial port a variável ReadLine
+const port = new SerialPort('COM5'); //Conecta a porta serial COM5. Veja a sua na IDE do Arduino -> Tools -> Port
+
+const parser = port.pipe(new Readline({delimiter: '\n'})); //Lê a linha apenas se uma nova linhas for inserida
+
+parser.on('data', (temp) => { //Na recepção dos dados = "On data retrieving"
+
+    console.log(temp);//Printa o dado recebido no console
+
+    var datahora = new Date();//Pega a data do sistema 
+
+    var data = datahora.getDate()+"/"+(Number(datahora.getMonth())+1)+"/"+datahora.getFullYear(); //Transforma em uma data legível 1/4/2019
+
+    var hora = (datahora.getHours())+":"+(datahora.getMinutes()); //Transforma em uma hora legível 15:00
+
+    io.sockets.emit('temp', {date: data, time: hora, temp:temp}); //Emite o objeto temp, com os atributos date, time e temp
 });
 
-io.on('connection', (socket) => {
-    console.log("Alguem acessou a página do gráfico >-<"); //show a log as a new client connects.
+io.on('connection', (socket) => {//É mostrado quando alguem se conecta
+    console.log("Alguem acessou a página do gráfico >-<"); 
 })
