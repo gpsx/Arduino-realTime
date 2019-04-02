da0d25cf11bc2d8eda0d0531403e698e6b32bc7a
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-04-02 14:45:19 -0300

    Charts Improves

--- configs/graficos.js
+++ configs/graficos.js
@@ -32,6 +32,16 @@ var t1 = new Chart(temper, {
             fontFamily: " 'ZCOOL XiaoWei', serif",
             fontStyle: "normal"
         },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    // Inclui a °C para o gráfico de Temperatura
+                    callback: function(value) {
+                        return value + '°C';
+                    }
+                }
+            }]
+        }
     }
     
 });
@@ -68,36 +78,16 @@ var u1 = new Chart(umid, {
                 fontFamily: " 'ZCOOL XiaoWei', serif",
                 fontStyle: "normal"
             },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        // Inclui a porcentagem para o gráfico de umidade
+                        callback: function(value) {
+                            return value + '%';
+                        }
+                    }
+                }]
+            }
     }
     
-});
-
-socket.on('temp', function(data) { //Assim q o temp é recebido
-
-    let dados = data.temp.split(","); //Tranforma as temperaturas recebidas em um vetor de duas posições e usa a virgula como separador
-    // o resultado dessa operação será dados = [ 0 : "temperatura", 1 : "umidade"] 
-
-    date.innerHTML = data.date; //Define a data
-
-    updateChart(t1, 0);
-    updateChart(u1, 1);
-
-    //Para evitar a repetição de código
-    function updateChart(id, arrayPosition) {
-        if(id.data.labels.length != 15) { //Se houver menos de 15 dados
-            id.data.labels.push(data.time);  //Coloca o horário no eixo X
-            id.data.datasets.forEach((dataset) => {
-                dataset.data.push(dados[arrayPosition]); //Adiciona a temperatura no eixo Y
-            });
-        }
-        else {
-            id.data.labels.shift(); //Remove o primeiro horário
-            id.data.labels.push(data.time); //Insere a data atual
-            id.data.datasets.forEach((dataset) => {
-                dataset.data.shift(); //Remove o primeiro registro de tempertura
-                dataset.data.push(dados[arrayPosition]); //Insere um novo registro de temperatura
-            });
-        }
-        id.update(); //Atualiza o gráfico 
-    }
 });
