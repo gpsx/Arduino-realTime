8e529eac747b049e083e6b4d251b97e7b5182ec1
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-28 21:39:36 -0300

    inserts ok

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ var path = require('path'); //Recupera o modulo path, que vem junto do express
 var sql = require('mssql');
 
 var server = app.listen(4000, () => { //Inicia o servidor na porta 4000
-    console.log("\n Recebendo solicitações na porta 4000...");
 })
 
 var io = require('socket.io')(server); //Recupera o modulo so socket.io e atrela o socket.io ao nosso servidor express.
 
-var sensores;
+var sensor;
 app.use(express.static('public')); //Send index.html page on GET /
 
 const readline = require('readline');
@@ -36,62 +35,62 @@ const config = {
 
 const SerialPort = require('serialport'); //Recupera o modulo Serial Port
 
-SerialPort.list().then(entradas_seriais => {
+// SerialPort.list().then(entradas_seriais => {
 
-    // este bloco trata a verificação de Arduino conectado (inicio)
+//     // este bloco trata a verificação de Arduino conectado (inicio)
 
-    var entradas_seriais_arduino = entradas_seriais.filter(entrada_serial => {
-        return entrada_serial.vendorId == 2341 && entrada_serial.productId == 43;
-    });
+//     var entradas_seriais_arduino = entradas_seriais.filter(entrada_serial => {
+//         return entrada_serial.vendorId == 2341 && entrada_serial.productId == 43;
+//     });
 
-    if (entradas_seriais_arduino.length != 1) {
-        throw new Error("Nenhum Arduino está conectado ou porta USB sem comunicação ou mais de um Arduino conectado");
-    }
+//     if (entradas_seriais_arduino.length != 1) {
+//         throw new Error("Nenhum Arduino está conectado ou porta USB sem comunicação ou mais de um Arduino conectado");
+//     }
 
-    return entradas_seriais_arduino[0].comName;
+//     return entradas_seriais_arduino[0].comName;
 
 
-    // este bloco trata a verificação de Arduino conectado (fim)
+//     // este bloco trata a verificação de Arduino conectado (fim)
 
-}).then(ArduinoCon => {
-    receiveSend(ArduinoCon);
-})
+// }).then(ArduinoCon => {
+//     receiveSend(ArduinoCon);
+// })
 
 function receiveSend(porta) {
     const Readline = SerialPort.parsers.Readline; // Atribui o metodo readline do serial port a variável ReadLine
-    const port = new SerialPort(`${porta}`); //Conecta a porta serial COM5. Veja a sua na IDE do Arduino -> Tools -> Port
+    const port = new SerialPort(`COM7`); //Conecta a porta serial COM5. Veja a sua na IDE do Arduino -> Tools -> Port
 
-    const parser = port.pipe(new Readline({delimiter: '\n'})); //Lê a linha apenas se uma nova linhas for inserida
+    const parser = port.pipe(new Readline({delimiter: '\r\n'})); //Lê a linha apenas se uma nova linhas for inserida
+    parser.on('data', (data) => { //Na recepção dos dados = "On data retrieving"
+    console.log(data);//Printa o dado recebido no console
+    ut = data.split(',');
+    temp = ut[0];
 
-    parser.on('data', (temp) => { //Na recepção dos dados = "On data retrieving"
-
-    console.log(temp);//Printa o dado recebido no console
-
+    console.log('Temperatura:',ut[0],'Umidade:',ut[1])
     var datahora = new Date();//Pega a data do sistema 
 
     var data = datahora.getDate()+"/"+(Number(datahora.getMonth())+1)+"/"+datahora.getFullYear(); //Transforma em uma data legível 1/4/2019
 
     var hora = (datahora.getHours())+":"+(datahora.getMinutes()); //Transforma em uma hora legível 15:00
 
-    io.sockets.emit('temp', {date: data, time: hora, temp:temp}); //Emite o objeto temp, com os atributos date, time e temp
+    io.sockets.emit('temp', {date: data, time: hora, temp:data}); //Emite o objeto temp, com os atributos date, time e temp
 
-    // sql.connect(config, err => {
-    //     // ... error checks
+    sql.connect(config, err => {
+        // ... error checks
      
-    //     const request = new sql.Request()
-    //     request.stream = true // You can set streaming differently for each request
-    //     request.query(`INSERT INTO Usuarios VALUES ('${register.name}', '${register.email}','${register.password}', ${register.fkIDclientes})`) // or request.execute(procedure)
+        const request = new sql.Request()
+        request.stream = true // You can set streaming differently for each request
+        request.query(`INSERT INTO Alerta(Temperatura, Umidade, Sensor_Id) VALUES ('${temp}', '${data[1]}', ${sensor.Id})`) // or request.execute(procedure)
      
-    //     request.on('error', err => {
-    //         res.send(err)
-    //     })
-    //     request.on('done', result => {
-    //         console.log(result);
-    //         res.redirect('/');
-    //         sql.close();
-    //     })
+        request.on('error', err => {
+            console.log(err)
+        })
+        request.on('done', result => {
+            console.log('Dados registrados com sucesso');
+            sql.close();
+        })
         
-    // })
+    })
 });
 }
 
@@ -100,13 +99,16 @@ function receiveSend(porta) {
 rl.question('Qual o código do seu sensor?', (answer) => {
     // TODO: Log the answer in a database
     sql.connect(config).then(() => {
-        return sql.query`Select * from Sensor where IdSensor = ${answer}`
+        return sql.query`Select * from Sensor where Codigo = ${answer}`
     }).then(result => {        
-        console.log(result.rowsAffected);
         if (result.rowsAffected > 0) {
+            sensor = result.recordset[0];
             console.log("Achou");
+            console.log("\nRecebendo solicitações na porta 4000...");
+            receiveSend();
         }else{
             console.log(`Sensor não encontrado` );
+            console.log("\nRecebendo solicitações na porta 4000...");
         }
         
         sql.close()
